efdac78317a60d6d7707090724f5cc306e0e9b85
Axel Poyet <axel.poyet@cern.ch>
2021-10-08 12:47:04 +0200

    Study without AB

---
+++ lattice/dev_lattice/config.py
@@ -0,0 +1,17 @@
+parameters = {
+
+    'ON_SEXT'       :       1,
+    'ON_OCT'        :       1,
+    'ON_RF'         :       1,
+    'NRJ_GeV'  :       2.4,
+    'DELTAP'   : 0.00095
+
+}
+
+
+settings = {
+
+    'SAVE_FIGS'   : False,
+    'SAVE_TWISS'  : False
+
+}
